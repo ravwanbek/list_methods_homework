9bdf74522a41fdcc6670e05630ec3cd16a66ba66
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-14 18:35:22 +0900

    8

--- List08.py
+++ List08.py
@@ -6,4 +6,22 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    not_apple=[]
+    if fruits[0]!='apple':
+        not_apple.insert(0,fruits[0])
+    
+    if fruits[1]!='apple':
+        not_apple.insert(1,fruits[1])
+
+    if fruits[2]!='apple':
+        not_apple.insert(2,fruits[2])
+
+    if fruits[3]!='apple':
+        not_apple.insert(3,fruits[3])
+    
+    if fruits[4]!='apple':
+        not_apple.insert(4,fruits[4])
+    
+    
+    return not_apple
+print(main(['apple','apple','orange','kiwi','apple']))
